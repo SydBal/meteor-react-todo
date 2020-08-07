7112f1feca8d14dff6745d9a6804b63c1e5e2663
SydBal <dominic.balassone@gmail.com>
2020-08-06 23:32:41 -0700

    added filter for hiding completed tasks

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTracker } from 'meteor/react-meteor-data';
 import { Task } from './Task';
 import { TaskForm } from './TaskForm';
 import Tasks from '/imports/api/tasks';
 
 export const App = () => {
+  const [hideCompleted, setHideCompleted] = useState(false);
+
+  const filter =
+    hideCompleted
+      ? {
+        $or: [
+          { isChecked: false },
+          { isChecked: undefined }
+        ]
+      } : {}
+
   // use  { sort: { createdAt: -1 } } to get newest entries first
-  const tasks = useTracker(() => Tasks.find({}, { sort: { createdAt: -1 } }).fetch());
+  const tasks = useTracker(() => Tasks.find(filter, { sort: { createdAt: -1 } }).fetch());
 
   const toggleChecked = ({ _id, isChecked }) => {
     Tasks.update(_id, {
@@ -19,10 +30,20 @@ export const App = () => {
   const deleteTask = ({ _id }) => Tasks.remove(_id);
 
   return (
-    <>
-      <h1>Welcome to Meteor!</h1>
-
-      <ul>
+    <div className="simple-todos-react">
+      <h1>Tasks!</h1>
+      <div className="filters">
+        <label>
+          <input
+            type="checkbox"
+            readOnly
+            checked={ Boolean(hideCompleted) }
+            onClick={() => setHideCompleted(!hideCompleted)}
+          />
+          Hide Completed
+        </label>
+      </div>
+      <ul className="tasks">
         { tasks.map(task => <Task
           key={ task._id }
           task={ task }
@@ -31,6 +52,6 @@ export const App = () => {
         />) }
       </ul>
       <TaskForm />
-    </>
+    </div>
   );
 };
