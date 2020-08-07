29694e08a10d10e3835e5f98495d48aee0ccf951
SydBal <dominic.balassone@gmail.com>
2020-08-06 22:12:53 -0700

    Added form and submittion logic for tasks

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import { useTracker } from 'meteor/react-meteor-data';
 import { Task } from './Task';
+import { TaskForm } from './TaskForm';
 import Tasks from '/imports/api/tasks';
 
 export const App = () => {
   const tasks = useTracker(() => Tasks.find({}).fetch());
- 
+
   return (
-    <div>
+    <>
       <h1>Welcome to Meteor!</h1>
- 
+
       <ul>
         { tasks.map(task => <Task key={ task._id } task={ task }/>) }
       </ul>
-    </div>
+      <TaskForm />
+    </>
   );
 };
