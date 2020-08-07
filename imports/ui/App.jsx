81e6d8172a8b2002c96f9947cea68f9514791f22
SydBal <dominic.balassone@gmail.com>
2020-08-06 22:33:11 -0700

    Adjusted component structure to make task names clickable and styled with a strikethrough when checkbox is selected

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -8,12 +8,20 @@ export const App = () => {
   // use  { sort: { createdAt: -1 } } to get newest entries first
   const tasks = useTracker(() => Tasks.find({}, { sort: { createdAt: -1 } }).fetch());
 
+  const toggleChecked = ({ _id, isChecked }) => {
+    Tasks.update(_id, {
+      $set: {
+        isChecked: !isChecked
+      }
+    })
+  };
+
   return (
     <>
       <h1>Welcome to Meteor!</h1>
 
       <ul>
-        { tasks.map(task => <Task key={ task._id } task={ task }/>) }
+        { tasks.map(task => <Task key={ task._id } task={ task } onCheckboxClick={toggleChecked} />) }
       </ul>
       <TaskForm />
     </>
--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
 
-export const Task = ({ task }) => {
-  return <li>{task.text}</li>
+export const Task = ({ task, onCheckboxClick }) => {
+  return (
+    <li>
+      <label>
+        <input
+          type="checkbox"
+          checked={ Boolean(task.isChecked) }
+          onClick={ () => onCheckboxClick(task) }
+          readOnly
+        />
+        <span>{ task.text }</span>
+      </label>
+    </li>
+  );
 };
